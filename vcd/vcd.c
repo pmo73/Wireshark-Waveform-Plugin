9cbd3ed667cb85d176bda4fe2b167266ab2f7ae4
Bjoern Petersen <bjoern2.petersen@t-online.de>
2024-10-01 13:59:10 +0200

    Install plugin VCDReader in wiretap folder and add first basic version without functionality

--- vcd/vcd.c
+++ vcd/vcd.c
@@ -1,103 +1,41 @@
+#include "config.h"
 #include "wiretap/wtap-int.h"
 #include "wiretap/file_wrappers.h"
 #include "string.h"
 
-void wtap_register_usbdump(void);
+#include <ws_version.h>
 
-#define USBDUMP_MAGIC 0x9a90000e
+WS_DLL_PUBLIC_DEF const gchar plugin_version[] = VERSION;
+WS_DLL_PUBLIC_DEF const int plugin_want_major = WIRESHARK_VERSION_MAJOR;
+WS_DLL_PUBLIC_DEF const int plugin_want_minor = WIRESHARK_VERSION_MINOR;
 
-/* Private data needed to read the file initially. */
-typedef struct {
-    guint16	version;
-    guint32 multiframe_size;
-    gboolean multiframe_overrun;
-} usbdump_info_t;
+void wtap_register_vcd(void);
 
 
-static gboolean usbdump_read(wtap *wth, wtap_rec *rec, Buffer *buf,
+static gboolean vcd_read(wtap *wth, wtap_rec *rec, Buffer *buf,
                              int *err, gchar **err_info,
                              gint64 *data_offset);
-static gboolean usbdump_seek_read(wtap *wth, gint64 seek_off,
+static gboolean vcd_seek_read(wtap *wth, gint64 seek_off,
                                   wtap_rec *rec, Buffer *buf,
                                   int *err, gchar **err_info);
-static gboolean usbdump_read_packet(wtap *wth, FILE_T fh,
+static gboolean vcd_read_packet(wtap *wth, FILE_T fh,
                                     wtap_rec *rec, Buffer *buf,
                                     int *err, gchar **err_info);
 
-static int usbdump_file_type_subtype;
+static int vcd_file_type_subtype;
 
 /*
- * Try to interpret a file as a usbdump formatted file.
+ * Try to interpret a file as a vcd formatted file.
  * Read relevant parts of the given file and collect information needed to
  * read the individual frames. Return value indicates whether or not this is
- * recognized as an usbdump file.
+ * recognized as an vcd file.
  */
 static wtap_open_return_val
-usbdump_open(wtap *wth, int *err, char **err_info)
+vcd_open(wtap *wth, int *err, char **err_info)
 {
-    guint32 magic;
-    guint16 version;
-    guint32 multiframe_size;
-    usbdump_info_t *usbdump_info;
-
-    /* Read in the number that should be at the start of a "usbdump" file */
-    if (!wtap_read_bytes(wth->fh, &magic, sizeof magic, err, err_info)) {
-        if (*err != WTAP_ERR_SHORT_READ)
-            return WTAP_OPEN_ERROR;
-        return WTAP_OPEN_NOT_MINE;
-    }
-
-    /* Check the file magic */
-    if (GUINT32_FROM_LE(magic) != USBDUMP_MAGIC)
-    {
-        return WTAP_OPEN_NOT_MINE;
-    }
-
-    /* Read the version of the header */
-    if (!wtap_read_bytes(wth->fh, &version, sizeof version, err, err_info)) {
-        if (*err != WTAP_ERR_SHORT_READ)
-            return WTAP_OPEN_ERROR;
-        return WTAP_OPEN_NOT_MINE;
-    }
-
-    /* Check for the supported version number */
-    if (GUINT16_FROM_BE(version) != 3) {
-        /* We only support version 0.3 */
-        *err = WTAP_ERR_UNSUPPORTED;
-        *err_info = g_strdup_printf("usbdump: version %u.%u unsupported",
-            version >> 8, version & 0xff);
-        return WTAP_OPEN_NOT_MINE;
-    }
-
-    /* Read the reserved field of the header */
-    if (!wtap_read_bytes(wth->fh, NULL, 26, err, err_info)) {
-        if (*err != WTAP_ERR_SHORT_READ)
-            return WTAP_OPEN_ERROR;
-        return WTAP_OPEN_NOT_MINE;
-    }
-
-    /* Read the initial multiframe size field */
-    if (!wtap_read_bytes(wth->fh, &multiframe_size, sizeof multiframe_size,
-        err, err_info)) {
-        if (*err != WTAP_ERR_SHORT_READ)
-            return WTAP_OPEN_ERROR;
-        return WTAP_OPEN_NOT_MINE;
-    }
-
-    /* Create a private structure to track the multiframe */
-    usbdump_info = (usbdump_info_t *)g_malloc(sizeof(usbdump_info_t));
-    usbdump_info->version = GUINT16_FROM_BE(version);
-    usbdump_info->multiframe_size = GUINT32_FROM_LE(multiframe_size);
-    usbdump_info->multiframe_overrun = FALSE;
-
-    /*
-     * We are convinced this is a usbdump format file.
-     * Setup the wiretap structure and fill it with info of this format.
-     */
-    wth->priv = (void *)usbdump_info;
-    wth->subtype_read = usbdump_read;
-    wth->subtype_seek_read = usbdump_seek_read;
-    wth->file_type_subtype = usbdump_file_type_subtype;
+    wth->subtype_read = vcd_read;
+    wth->subtype_seek_read = vcd_seek_read;
+    wth->file_type_subtype = vcd_file_type_subtype;
     wth->file_encap = WTAP_ENCAP_USB_FREEBSD;
     wth->file_tsprec = WTAP_TSPREC_USEC;
 
@@ -111,37 +49,15 @@ usbdump_open(wtap *wth, int *err, char **err_info)
  * support subsequent random access read.
  */
 static gboolean
-usbdump_read(wtap *wth, wtap_rec *rec, Buffer *buf, int *err, gchar **err_info,
+vcd_read(wtap *wth, wtap_rec *rec, Buffer *buf, int *err, gchar **err_info,
              gint64 *data_offset)
 {
-    usbdump_info_t *usbdump_info = (usbdump_info_t *)wth->priv;
-
     /* Report the current file location */
     *data_offset = file_tell(wth->fh);
 
     /* Try to read a packet worth of data */
-    if (!usbdump_read_packet(wth, wth->fh, rec, buf, err, err_info))
+    if (!vcd_read_packet(wth, wth->fh, rec, buf, err, err_info))
         return FALSE;
-
-    /* Check if we overrun the multiframe during the last read */
-    if (usbdump_info->multiframe_overrun)
-    {
-        *err = WTAP_ERR_BAD_FILE;
-        *err_info = "Multiframe overrun";
-        return FALSE;
-    }
-
-    /* See if we reached the end of the multiframe */
-    if (usbdump_info->multiframe_size == 0)
-    {
-        /*
-         * Try to read the subsequent multiframe size field.
-         * This will fail at end of file, but that is accepted.
-         */
-        wtap_read_bytes_or_eof(wth->fh, &usbdump_info->multiframe_size,
-                               sizeof usbdump_info->multiframe_size,
-                               err, err_info);
-    }
 
     return TRUE;
 }
@@ -152,7 +68,7 @@ usbdump_read(wtap *wth, wtap_rec *rec, Buffer *buf, int *err, gchar **err_info,
  * in a buffer and fill in the packet header info.
  */
 static gboolean
-usbdump_seek_read(wtap *wth, gint64 seek_off, wtap_rec *rec,
+vcd_seek_read(wtap *wth, gint64 seek_off, wtap_rec *rec,
                   Buffer *buf, int *err, gchar **err_info)
 {
     /* Seek to the desired file position at the start of the frame */
@@ -160,7 +76,7 @@ usbdump_seek_read(wtap *wth, gint64 seek_off, wtap_rec *rec,
         return FALSE;
 
     /* Try to read a packet worth of data */
-    if (!usbdump_read_packet(wth, wth->random_fh, rec, buf, err, err_info)) {
+    if (!vcd_read_packet(wth, wth->random_fh, rec, buf, err, err_info)) {
         if (*err == 0)
             *err = WTAP_ERR_SHORT_READ;
         return FALSE;
@@ -179,11 +95,9 @@ usbdump_seek_read(wtap *wth, gint64 seek_off, wtap_rec *rec,
  * so that we can find the next multiframe size field.
  */
 static gboolean
-usbdump_read_packet(wtap *wth, FILE_T fh, wtap_rec *rec, Buffer *buf,
+vcd_read_packet(wtap *wth, FILE_T fh, wtap_rec *rec, Buffer *buf,
                     int *err, gchar **err_info)
 {
-    usbdump_info_t *usbdump_info = (usbdump_info_t *)wth->priv;
-
     guint8 bpf_hdr[18];
     guint8 bpf_hdr_len, alignment;
 
@@ -203,13 +117,6 @@ usbdump_read_packet(wtap *wth, FILE_T fh, wtap_rec *rec, Buffer *buf,
             return FALSE;
     }
 
-    /* Keep track of multiframe_size and detect overrun */
-    if (usbdump_info->multiframe_size < bpf_hdr_len) {
-        usbdump_info->multiframe_overrun = TRUE;
-    } else {
-        usbdump_info->multiframe_size -= bpf_hdr_len;
-    }
-
     /* Setup the per packet structure and fill it with info from this frame */
     rec->rec_type = REC_TYPE_PACKET;
     rec->presence_flags = WTAP_HAS_TS | WTAP_HAS_CAP_LEN;
@@ -226,13 +133,6 @@ usbdump_read_packet(wtap *wth, FILE_T fh, wtap_rec *rec, Buffer *buf,
     if (!wtap_read_packet_bytes(fh, buf, rec->rec_header.packet_header.caplen, err, err_info))
         return FALSE;
 
-    /* Keep track of multiframe_size and detect overrun */
-    if (usbdump_info->multiframe_size < rec->rec_header.packet_header.caplen) {
-        usbdump_info->multiframe_overrun = TRUE;
-    } else {
-        usbdump_info->multiframe_size -= rec->rec_header.packet_header.caplen;
-    }
-
     /* Check for and apply alignment as defined in the frame header */
     guint8 pad_len = (guint32)alignment -
                      (((guint32)bpf_hdr_len + rec->rec_header.packet_header.caplen) &
@@ -241,13 +141,6 @@ usbdump_read_packet(wtap *wth, FILE_T fh, wtap_rec *rec, Buffer *buf,
         /* Read alignment from the file */
         if (!wtap_read_bytes(fh, NULL, pad_len, err, err_info))
             return FALSE;
-
-        /* Keep track of multiframe_size and detect overrun */
-        if (usbdump_info->multiframe_size < pad_len) {
-            usbdump_info->multiframe_overrun = TRUE;
-        } else {
-            usbdump_info->multiframe_size -= pad_len;
-        }
     }
 
     return TRUE;
@@ -256,15 +149,15 @@ usbdump_read_packet(wtap *wth, FILE_T fh, wtap_rec *rec, Buffer *buf,
 /*
  * Register with wiretap.
  * Register how we can handle an unknown file to see if this is a valid
- * usbdump file and register information about this file format.
+ * vcd file and register information about this file format.
  */
 void
-wtap_register_usbdump(void)
+wtap_register_vcd(void)
 {
     struct open_info oi = {
-        "FreeBSD usbdump",
-        OPEN_INFO_MAGIC,
-        usbdump_open,
+        "Value Change Dump",
+        OPEN_INFO_HEURISTIC,
+        vcd_open,
         NULL,
         NULL,
         NULL
@@ -273,9 +166,9 @@ wtap_register_usbdump(void)
     wtap_register_open_info(&oi, FALSE);
 
     struct file_type_subtype_info fi = {
-        "FreeBSD USBDUMP",
-        "usbdump",
-        NULL,
+        "Value Change Dump",
+        "VCD",
+        "vcd",
         NULL,
         FALSE,
         FALSE,
@@ -285,7 +178,15 @@ wtap_register_usbdump(void)
         NULL
     };
 
-    usbdump_file_type_subtype =
-        wtap_register_file_type_subtypes(&fi, WTAP_FILE_TYPE_SUBTYPE_UNKNOWN);
+    vcd_file_type_subtype =
+        wtap_register_file_type_subtype(&fi);
 }
 
+WS_DLL_PUBLIC
+void plugin_register(void)
+{
+    static wtap_plugin plug_vcd;
+
+    plug_vcd.register_wtap_module = wtap_register_vcd;
+    wtap_register_plugin(&plug_vcd);
+}
