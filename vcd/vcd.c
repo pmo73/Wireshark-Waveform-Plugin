aff9864d71667d0da43d19c57a88e0d5f7cd8e52
Bjoern Petersen <bjoern2.petersen@t-online.de>
2024-10-01 14:00:51 +0200

    Remove unused include

--- vcd/vcd.c
+++ vcd/vcd.c
@@ -3,11 +3,9 @@
 #include "wiretap/file_wrappers.h"
 #include "string.h"
 
-#include <ws_version.h>
-
 WS_DLL_PUBLIC_DEF const gchar plugin_version[] = VERSION;
-WS_DLL_PUBLIC_DEF const int plugin_want_major = WIRESHARK_VERSION_MAJOR;
-WS_DLL_PUBLIC_DEF const int plugin_want_minor = WIRESHARK_VERSION_MINOR;
+WS_DLL_PUBLIC_DEF const int plugin_want_major = VERSION_MAJOR;
+WS_DLL_PUBLIC_DEF const int plugin_want_minor = VERSION_MINOR;
 
 void wtap_register_vcd(void);
 
